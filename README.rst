d8eca14469b2fc89edee5add2ab08664cffa30e3
Stéphane RAULT <stephane.rault@radicalspam.org>
2015-02-17 17:09:58 +0100

    auto-update [skip ci]

---
+++ README.rst
@@ -0,0 +1,14 @@
+Docker example for Websocketd behind Nginx Proxy
+================================================
+
+
+Run
+---
+
+::
+
+    git clone https://github.com/srault95/docker-websocketd.git
+    
+    cd docker-websocketd
+    
+    docker build -t srault95/websocketd
--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ echo "--> start.sh script running..."
 
 printf "${DEFAULT_USER:-root}:$(openssl passwd -apr1 ${DEFAULT_PASSWORD:-root})\n" > /etc/nginx/htpasswd
 
-/usr/local/bin/websocketd --port=9231 --staticdir=/usr/local/web-vmstats/web /usr/bin/vmstat -n 1 &
+/usr/local/bin/websocketd --port=8010 --staticdir=/usr/local/web-vmstats/web /usr/bin/vmstat -n 1 &
 
 exec /usr/sbin/nginx 
